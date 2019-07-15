926256d01b72cf367976ef9c648d50556a37b0e1
Seth Miller <seth@sethmiller.me>
2019-07-14 22:18:08 -0500

    increase window width to 650

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.version = "11.2"
     t.window.vsync = false
     t.console = false
-    t.window.width = 600
+    t.window.width = 650
     t.window.height = 800
     t.window.title = "Tetris"
 end
